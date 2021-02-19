a84faea27ee372105688e859eded44d0ed274b7b
Maha <mahmalzahrany@gmail.com>
2021-02-19 18:13:20 +0300

    Database connection

---
+++ db/database.js
@@ -0,0 +1,13 @@
+require("custom-env").env(true);
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
